534f88f7977b311989103d9acdebf1ffa89fad4a
kapilverma12 <105623322+kapilverma12@users.noreply.github.com>
2025-07-20 16:23:56 +0530

    fixed template

--- resources/views/emails/contract.blade.php
+++ resources/views/emails/contract.blade.php
@@ -1,6 +1,6 @@
 <html>
 <body>
-    <p>Dear {{ $record->full_name ?? 'User' }},</p>
+    <p>Dear {{ $record->customer->full_name ?? 'User' }},</p>
     <p>Please find your contract attached to this email.</p>
     <p>Regards,<br>Falcon Consultant</p>
 </body>
